df85b8bdd45ce6f76c3727d9d095a54843991fa3
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2023-10-17 20:27:39 -0400

    feat: background display

---
+++ code/main.py
@@ -0,0 +1,49 @@
+from settings import (
+    CELL_SIZE,
+    COLS,
+    DARK_GREEN,
+    LIGHT_GREEN,
+    ROWS,
+    WINDOW_HEIGHT,
+    WINDOW_WIDTH,
+    exit,
+    pygame,
+)
+
+
+class Main:
+    def __init__(self):
+        # general
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("PySnake")
+        # game objects
+        self.bg_rectangles = [
+            pygame.Rect(
+                (col + int(row % 2 == 0)) * CELL_SIZE,  # left
+                row * CELL_SIZE,  # top
+                CELL_SIZE,  # width
+                CELL_SIZE,  # height
+            )
+            for col in range(0, COLS, 2)
+            for row in range(ROWS)
+        ]
+
+    def draw_bg(self):
+        for rect in self.bg_rectangles:
+            pygame.draw.rect(self.display_surface, DARK_GREEN, rect)
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+            self.display_surface.fill(LIGHT_GREEN)
+            self.draw_bg()
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
