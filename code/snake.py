af9ca08bd52022c4c014559610c231d80c4052de
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2023-10-21 00:20:01 -0400

    feat(Snake): load graphics and render head

--- code/snake.py
+++ code/snake.py
@@ -1,10 +1,5 @@
-from settings import (
-    CELL_SIZE,
-    START_COL,
-    START_LENGTH,
-    START_ROW,
-    pygame,
-)
+from os import walk
+from settings import CELL_SIZE, START_COL, START_LENGTH, START_ROW, pygame, join
 
 
 class Snake:
@@ -15,6 +10,19 @@ class Snake:
         ]
         self.direction = pygame.Vector2(1, 0)
         self.has_eaten = False
+        # graphics
+        self.surfaces = self.import_surfs()
+        self.draw_data = []
+        self.head_surf = self.surfaces["head_right"]
+
+    def import_surfs(self):
+        surf_dict = {}
+        for folder_path, _, images_name in walk(join("graphics", "snake")):
+            for image_name in images_name:
+                full_path = join(folder_path, image_name)
+                image_surface = pygame.image.load(full_path).convert_alpha()
+                surf_dict[image_name.split(".")[0]] = image_surface
+        return surf_dict
 
     def reset(self):
         self.body = [
@@ -35,6 +43,30 @@ class Snake:
             self.body = body_copy[:]
             self.has_eaten = False
 
+        self.update_head()
+        self.update_body()
+
+    def update_head(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == pygame.Vector2(-1, 0):
+            self.head_surf = self.surfaces["head_right"]
+        elif head_relation == pygame.Vector2(1, 0):
+            self.head_surf = self.surfaces["head_left"]
+        elif head_relation == pygame.Vector2(0, -1):
+            self.head_surf = self.surfaces["head_down"]
+        elif head_relation == pygame.Vector2(0, 1):
+            self.head_surf = self.surfaces["head_up"]
+
+    def update_body(self):
+        self.draw_data = []
+        for index, part in enumerate(self.body):
+            x = part.x * CELL_SIZE
+            y = part.y * CELL_SIZE
+            rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
+
+            if index == 0:
+                self.draw_data.append((self.head_surf, rect))
+
     def draw(self):
         for point in self.body:
             rect = pygame.Rect(
@@ -44,3 +76,6 @@ class Snake:
                 CELL_SIZE,
             )
             pygame.draw.rect(self.display_surface, "red", rect)
+
+        for surf, rect in self.draw_data:
+            self.display_surface.blit(surf, rect)
